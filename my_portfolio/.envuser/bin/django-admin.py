677d5e47b5390ac92b6d98cf15833109ae14d0d6
mpelszyk <marcin.pelszyk90@gmail.com>
2021-04-11 18:58:20 +0200

    add blog project

---
+++ my_portfolio/.envuser/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mpelszyk/Pulpit/GitHub/PYTHON/Django.f/django-protfolio/my_portfolio/.envuser/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ my_portfolio/awesome_website/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('accounts/', include("django.contrib.auth.urls")),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    path('oauth/', include("social_django.urls")),
+]
