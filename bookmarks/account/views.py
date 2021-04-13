bc2500ba27370606c27077eb0d4668f2208189db
mpelszyk <marcin.pelszyk90@gmail.com>
2021-04-13 09:37:59 +0200

    bookmarks project

---
+++ bookmarks/account/views.py
@@ -0,0 +1,38 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .fomrs import LoginForm
+from django.contrib.auth import views as aut_views
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+    
+
+# def user_login(request):
+
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(username=cd['username'],
+#                                 password=cd['password'])
+
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request, user)
+#                     return HttpResponse('Uwierzytelnienie zakończLo się sukcesem.')
+#                 else:
+#                     return HttpResponse('Konto jest zablokowane')
+#             else:
+#                 return HttpResponse('Nieprawidłowe dane uwerzytelniające.')
+#     else:
+#         form = LoginForm()
+#     return render(request, 'account/login.html', {'form': form})
+
+
+
+
+
