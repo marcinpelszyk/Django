9579ed1c1e30dd8d3f6cdfc0f31e530051860ca8
mpelszyk <marcin.pelszyk90@gmail.com>
2021-04-15 14:32:26 +0200

    initial commit

---
+++ core/shop/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+    
+    # def get_absolute_url(self):
+    #     return reverse('shop:product_list_by_category', args=[self.slug])
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.CASCADE,
+         related_name='products')
+    created_by = models.ForeignKey(
+        User, 
+        related_name='product_create',
+        on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=255, default='admin')
+    slug = models.SlugField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ('-created',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.title
+
+    # def get_absolute_url(self):
+    #     return reverse("shop:product_detail", args=[self.pk, self.slug])
+
+
+    
+    
+
+
+
+
---
+++ core/shop/tests/test_models.py
@@ -0,0 +1,64 @@
+from django.test import TestCase
+from django.urls import reverse
+from shop.models import Category, Product
+from django.contrib.auth.models import User
+
+class TestCategoriesModel(TestCase):
+
+    def setUp(self):
+
+        self.data1 = Category.objects.create(name='wodka', slug='wodka')
+
+    def test_category_model_entry(self):
+        """
+        Test Category model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Category))
+        self.assertEqual(str(data), 'wodka')
+
+    def test_category_url(self):
+        """
+        Test category model slug and URL reverse
+        """
+        data = self.data1
+        response = self.client.post(
+            reverse('shop:product_list_by_category', args=[data.slug]))
+        self.assertEqual(response.status_code, 200)
+
+
+
+class TestProductsModel(TestCase):
+    def setUp(self):
+        Category.objects.create(name='wodka2', slug='wodka2')
+        User.objects.create(username='admin')
+        self.data1 = Product.objects.create(category_id=1, title='wodka2', created_by_id=1,
+                                            slug='wodka2', price='20.00', image='wodka2')
+        self.data2 = Product.products.create(category_id=1, title='grass', created_by_id=1,
+                                             slug='grass', price='20.00', image='grass', is_active=False)
+
+    def test_products_model_entry(self):
+        """
+        Test product model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Product))
+        self.assertEqual(str(data), 'wodka2')
+
+    def test_products_url(self):
+        """
+        Test product model slug and URL reverse
+        """
+        data = self.data1
+        url = reverse('shop:product_detail', args=[data.slug])
+        self.assertEqual(url, '/item/wokda2/')
+        response = self.client.post(
+            reverse('shop:product_detail', args=[data.slug]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_products_custom_manager_basic(self):
+        """
+        Test product model custom manager returns only active products
+        """
+        data = Product.products.all()
+        self.assertEqual(data.count(), 1)
