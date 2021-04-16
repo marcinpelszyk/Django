c9c620420adf9ad93f43e78bd5288bec0fb21c40
mpelszyk <marcin.pelszyk90@gmail.com>
2021-04-16 15:02:37 +0200

    add test model and home page

--- core/shop/models.py
+++ core/shop/models.py
@@ -14,8 +14,8 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
-    # def get_absolute_url(self):
-    #     return reverse('shop:product_list_by_category', args=[self.slug])
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
 
 class Product(models.Model):
--- core/shop/views.py
+++ core/shop/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Category, Product
+
+
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'shop/home.html', {'products': products})
+
+
+
+
+
+
+
+
