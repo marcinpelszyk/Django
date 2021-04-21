badf01a65155d949c61984a57267ceedafd6cc09
mpelszyk <marcin.pelszyk90@gmail.com>
2021-04-21 18:27:12 +0200

    Testing Sesions and ajax request

--- Sklep zielarski/store/tests/test_view.py
+++ Sklep zielarski/store/tests/test_view.py
@@ -1,19 +1,25 @@
+from importlib import import_module
 from unittest import skip
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.http import HttpRequest
-from django.test import Client, RequestFactory, TestCase
+from django.test import Client, TestCase
 from django.urls import reverse
 
 from store.models import Category, Product
 from store.views import product_all
+
+@skip('test skipping')
+class TestSkip(TestCase):
+    def test_skip_exmaple(self):
+        pass
 
 
 class TestViewResponse(TestCase):
 
     def setUp(self):
         self.c = Client()
-        self.factory = RequestFactory()
         User.obejcts.create(username='admin')
         Category.objects.create(name='olej', slug='olej')
         Product.objects.create(category_id=5, title='miod', created_by_id=5,
@@ -26,11 +32,13 @@ class TestViewResponse(TestCase):
         response = self.c.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_url_hosts(self):
+    def test_url_allowed_hosts(self):
         """
-        Test hosts URL
+        Test allowed hosts
         """
-        response = self.c.get('/')
+        response = self.c.get('/', HTTP_HOST='addres.pl')
+        self.assertEqual(response.status_code, 400)
+        response = self.c.get('/', HTTP_HOST='domena.pl')
         self.assertEqual(response.status_code, 200)
 
     def test_product_detail_url(self):
@@ -47,23 +55,16 @@ class TestViewResponse(TestCase):
         response = self.c.get(reverse('store:category_list', args=['olej']))
         self.assertEqual(response.status_code, 200)
 
-    def test_url_allowed_hosts(self):
-        """
-        Test allowed hosts
-        """
-        response = self.c.get('/', HTTP_HOST='addres.pl')
-        self.assertEqual(response.status_code, 400)
-        response = self.c.get('/', HTTP_HOST='domena.pl')
-        self.assertEqual(response.status_code, 200)
-
     def test_homepage_html(self):
         """
-        Code validation search HTML for text
+        Code validation, search HTML for text
         """
         request = HttpRequest()
+        engine = import_module(settings.SESSION_ENGINE)
+        request.session = engine.SessionStore()
         response = product_all(request)
         html = response.content.decode('utf8')
-        self.assertIn('<title>Home</title>', html)
+        self.assertIn('<title> Zielarz </title>', html)
         self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
         self.assertEqual(response.status_code, 200)
 
